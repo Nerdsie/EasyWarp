5274da47863ffcbcaaa9f4a373fef43c40215f6e
nerdsie <richcoll97@gmail.com>
2024-01-03 19:36:38 -0800

    Include cooldown mechanism

--- src/main/java/com/collinsrichard/easywarp/objects/WarpTimer.java
+++ src/main/java/com/collinsrichard/easywarp/objects/WarpTimer.java
@@ -1,24 +1,31 @@
 package com.collinsrichard.easywarp.objects;
 
 import com.collinsrichard.easywarp.Helper;
+import com.collinsrichard.easywarp.Settings;
+import com.collinsrichard.easywarp.managers.CooldownManager;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class WarpTimer implements Runnable {
 	public Player player = null;
 	public Warp warp = null;
+	public boolean triggerCooldown = false;
 
 	public int id = 0;
 
-	public WarpTimer(Player p, Warp w) {
+	public WarpTimer(Player p, Warp w, boolean triggerCooldown) {
 		player = p;
 		warp = w;
+		this.triggerCooldown = triggerCooldown;
 	}
 
 	@Override
 	public void run() {
 		Helper.stopWarping(player);
 
+		if(triggerCooldown && !Helper.playerBypassesDelays(player)) {
+			CooldownManager.setCooldown(player, Settings.cooldown);
+		}
         Helper.warp(player, warp);
 	}
 
